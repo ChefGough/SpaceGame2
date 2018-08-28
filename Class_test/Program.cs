1dd73d9b59a7633baf1ee9b55b697417bf3a86bb
Alexander Gough <gougha3@my.erau.edu>
2018-08-28 09:49:11 -0400

    Add project files.

---
+++ Class_test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

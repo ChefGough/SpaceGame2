990a8e1c70967643af835ab768f414c5781bf88a
Alexander Gough <gougha3@my.erau.edu>
2018-08-30 14:26:53 -0400

    If applied this commit will: Start the warp drive function.

--- Class_test/WarpDrive.cs
+++ Class_test/WarpDrive.cs
@@ -12,19 +12,20 @@ namespace Class_test
         static Double yearsTraveled = 0;
         //Warp level for upgrade and travel calculation
         static int warpLevel = 2;
+        static double TimeYears = 0;
 
         public void WarpUpgrade()
         {
             warpLevel += 1;
         }
-        /*public void WarpTravel()
+       public void WarpTravelEarth()
         {
             double Speed;
             Speed = Math.Pow(warpLevel, (10.0 / 3)) + Math.Pow(10 - warpLevel, (-11 / 3.0));
             TimeYears = 4.0 / Speed;
             Console.WriteLine("You have travled for: " + TimeYears + " Years");
             Console.ReadLine();
-        }*/
+        }
 
 }
 }
